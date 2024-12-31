3e592206a15e365f6ddafa1982aa54eefedaee50
@kammopajhb024 <mmopanemoroka@gmail.com>
2024-12-31 14:54:00 +0200

    anonymous

---
+++ src/Machine.java
@@ -0,0 +1,23 @@
+//Anonymous Classes helps me extend classes on the fly
+
+class Machine {
+    public void start() {
+        System.out.println("woooosh");
+    }
+}
+
+// Subclass that overrides the start method
+class AdvancedMachine extends Machine {
+    @Override
+    public void start() {
+        System.out.println("starting...");
+    }
+}
+
+class StartProgram {
+    public static void main(String[] args) {
+        Machine m = new AdvancedMachine();
+        Machine m2 = new AdvancedMachine();// Upcasting
+        m2.start(); // Output: "starting..."
+    }
+}
