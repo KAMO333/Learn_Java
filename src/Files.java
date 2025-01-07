e924e4eca441f431d7da5ca1e3c799c7877774ae
@kammopajhb024 <mmopanemoroka@gmail.com>
2025-01-07 15:43:35 +0200

    working with fies

---
+++ src/Files.java
@@ -0,0 +1,21 @@
+//Working with files:
+//Finding the file
+
+
+import java.io.File;
+//
+//class MyClass {
+//    public static void main(String[ ] args) {
+//        File file = new File("/home/kamogelo-mmopane/Documents/Code Projects/Github/Learn Java/src/Example");
+//        if(file.exists()) {
+//            System.out.println(file.getName() +  " " + "exists!");
+//        }
+//        else {
+//            System.out.println("The file does not exist");
+//        }
+//    }
+//}
+
+//READING A FILE:
+
+
