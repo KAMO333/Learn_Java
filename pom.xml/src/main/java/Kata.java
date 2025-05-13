e06286f9eaf868d7d7bfa8fc9d90288b8d5be57d
KAMO333 <mmopanemoroka@gmail.com>
2025-05-13 10:33:58 +0200

    remove spaces from string

--- pom.xml/src/main/java/Kata.java
+++ pom.xml/src/main/java/Kata.java
@@ -2,4 +2,8 @@ public class Kata {
     public static String numberToString(int num) {
         return String.valueOf(num);
     };
+
+    public static String noSpace(final String x) {
+        return "";
+    }
 }
