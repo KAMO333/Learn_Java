95ad2f39356b8d99878d3590965e9b3a6798ba46
Kamogelo <mmopanemoroka@gmail.com>
2025-05-15 23:01:59 +0200

    looping array 2

--- pom.xml/src/main/java/SumArray.java
+++ pom.xml/src/main/java/SumArray.java
@@ -1,6 +1,14 @@
 public class SumArray {
 
     public static double sum(double[] numbers) {
+        double results = 0.0;
+
+
+        for(int i = 0; i < numbers.length; i++) {
+            results += numbers[i];
+        }
+
+        return results;
 
     }
 }
