24b21f059986957f8ba9ad88eb0093b1693e838f
Kamogelo <mmopanemoroka@gmail.com>
2025-05-16 14:26:20 +0200

    key value oop

---
+++ pom.xml/src/main/java/Dictonary.java
@@ -0,0 +1,13 @@
+public class Dictionary {
+    public Dictionary(){
+        // your code
+    }
+
+    public void newEntry(String key, String value){
+        // your code
+    }
+
+    public String look(String key){
+        // your code
+    }
+}
