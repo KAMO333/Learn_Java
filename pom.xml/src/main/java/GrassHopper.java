0396b2077ceb17134019c1726cfc741447815720
Kamogelo <mmopanemoroka@gmail.com>
2025-05-18 16:55:56 +0200

    solved

--- pom.xml/src/main/java/GrassHopper.java
+++ pom.xml/src/main/java/GrassHopper.java
@@ -1,6 +1,10 @@
 public class GrassHopper {
     public static int summation(int n) {
+        int counter = 0;
 
-        return 0;
+        for(int i = 1; i <= n; i += 1) {
+            counter += i;
+        }
+        return counter;
     }
 }
