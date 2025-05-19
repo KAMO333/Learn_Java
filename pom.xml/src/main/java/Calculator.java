63b2293fcb60a4a196779cc54672e272ef7f91d3
Kamogelo <mmopanemoroka@gmail.com>
2025-05-19 22:53:37 +0200

    my tests

---
+++ pom.xml/src/main/java/Calculator.java
@@ -0,0 +1,63 @@
+public class Calculator {
+
+    // Adds two numbers
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    // Subtracts second number from first
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    // Multiplies two numbers
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    // Divides first number by second
+    // Throws ArithmeticException if b is 0
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new ArithmeticException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+
+    // Checks if a number is even
+    public boolean isEven(int number) {
+        return number % 2 == 0;
+    }
+
+    // Returns the maximum of two numbers
+    public int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    // Calculates factorial of a number
+    // Throws IllegalArgumentException for negative input
+    public int factorial(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("Factorial is not defined for negative numbers");
+        }
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    // Checks if a string is a palindrome
+    public boolean isPalindrome(String str) {
+        if (str == null) {
+            return false;
+        }
+        String cleanStr = str.replaceAll("\\s+", "").toLowerCase();
+        int length = cleanStr.length();
+        for (int i = 0; i < length / 2; i++) {
+            if (cleanStr.charAt(i) != cleanStr.charAt(length - 1 - i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
