66a9c328b7058057ab658daca24c6db87045b323
Kamogelo <mmopanemoroka@gmail.com>
2025-05-19 23:15:28 +0200

    continue

--- pom.xml/src/main/java/Calculator.java
+++ pom.xml/src/main/java/Calculator.java
@@ -1,23 +1,23 @@
 public class Calculator {
 
     // Adds two numbers
-    public int add(int a, int b) {
+    public static int add(int a, int b) {
         return a + b;
     }
 
     // Subtracts second number from first
-    public int subtract(int a, int b) {
+    public static int subtract(int a, int b) {
         return a - b;
     }
 
     // Multiplies two numbers
-    public int multiply(int a, int b) {
+    public static int multiply(int a, int b) {
         return a * b;
     }
 
     // Divides first number by second
     // Throws ArithmeticException if b is 0
-    public double divide(int a, int b) {
+    public static double divide(int a, int b) {
         if (b == 0) {
             throw new ArithmeticException("Cannot divide by zero");
         }
--- pom.xml/src/test/java/CalculatorTests.java
+++ pom.xml/src/test/java/CalculatorTests.java
@@ -1,2 +1,36 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class CalculatorTests {
+
+    @Test
+    public void adding() {
+        assertEquals(2, Calculator.add(1, 1));
+    }
+
+    @Test
+    public void adding2() {
+        assertNotEquals(1, Calculator.add(1, 1));
+    }
+
+    @Test
+    public void sub() {
+        assertEquals(1, Calculator.subtract(4, 3));
+    }
+
+    @Test
+    public void sub2() {
+        assertNotEquals(2, Calculator.subtract(1, 1));
+    }
+
+    @Test
+    public void mult() {
+        assertEquals(4, Calculator.multiply(2,2));
+    }
+
+    @Test
+    public void div() {
+        assertThat(4).isCloseTo(3.0, within(1e-5));
+    }
 }
