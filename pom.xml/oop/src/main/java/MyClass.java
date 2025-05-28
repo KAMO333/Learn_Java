85a3b6961883fcfb7657c22ae10fd05be7669f59
Kamogelo <mmopanemoroka@gmail.com>
2025-05-28 14:26:13 +0200

    object creation 2

--- pom.xml/oop/src/main/java/MyClass.java
+++ pom.xml/oop/src/main/java/MyClass.java
@@ -1,6 +1,8 @@
 public class MyClass {
     public static void main(String[] args) {
         Animal dog = new Animal();
+        Vehicle car = new Vehicle();
         dog.bark();
+        car.horn();
     }
 }
