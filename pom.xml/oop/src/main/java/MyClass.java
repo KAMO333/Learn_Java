e4238cf2336c99a8e1a9aa6c11a6d97a5b0a2f60
Kamogelo <mmopanemoroka@gmail.com>
2025-05-29 15:27:36 +0200

    value types

--- pom.xml/oop/src/main/java/MyClass.java
+++ pom.xml/oop/src/main/java/MyClass.java
@@ -9,8 +9,16 @@ public class MyClass {
         car2.horn();
         System.out.println(car2.getColor());
 
+        int x = 5;
+        addOneTo(x);
+        System.out.println(x);
+
         Counter c1 = new Counter();
         Counter c2 = new Counter();
         System.out.println(Counter.COUNT);
+    }
+
+    static void addOneTo(int num) {
+        num = num + 1;
     }
 }
