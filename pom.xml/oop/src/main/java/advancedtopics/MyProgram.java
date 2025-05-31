2750d5903ec7885ae225fbfe727c9b65c4d23a2f
Kamogelo <mmopanemoroka@gmail.com>
2025-05-31 17:18:29 +0200

    type casting

---
+++ pom.xml/oop/src/main/java/advancedtopics/MyProgram.java
@@ -0,0 +1,11 @@
+package advancedtopics;
+
+public class MyProgram {
+    public static void main(String[] args) {
+        double num = 4.765;
+        int number = (int) num;
+        String n = String.valueOf(number);
+
+        System.out.println(number);
+    }
+}
