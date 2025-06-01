204d3c173f72dbcfc6bcb2edfafe0258f1c4c055
Kamogelo <mmopanemoroka@gmail.com>
2025-06-01 14:41:55 +0200

    inner class

---
+++ pom.xml/oop/src/main/java/advancedtopics/MyOtherProgram.java
@@ -0,0 +1,7 @@
+package advancedtopics;
+
+public class MyOtherProgram {
+    public static void main(String[] args) {
+        Robot r = new Robot(10);
+    }
+}
---
+++ pom.xml/oop/src/main/java/advancedtopics/Robot.java
@@ -0,0 +1,18 @@
+package advancedtopics;
+
+public class Robot {
+    int id;
+
+    Robot(int i) {
+        id = i;
+        Brain b = new Brain();
+        b.think();
+    }
+
+    private class Brain {
+        public void think() {
+            System.out.println(id + " says I'm thinking....");
+        }
+
+    }
+}
