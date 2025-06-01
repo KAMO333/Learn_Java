5649a859de448d98c113da402122564505607ad0
Kamogelo <mmopanemoroka@gmail.com>
2025-06-01 15:20:36 +0200

    equality and hashcode overriding

--- pom.xml/oop/src/main/java/advancedtopics/MyOtherProgram.java
+++ pom.xml/oop/src/main/java/advancedtopics/MyOtherProgram.java
@@ -3,5 +3,10 @@ package advancedtopics;
 public class MyOtherProgram {
     public static void main(String[] args) {
         Robot r = new Robot(10);
+
+        Zoo monkey = new Zoo("mike123");
+        Zoo donkey = new Zoo("mike123");
+
+        System.out.println(monkey.equals(donkey));
     }
 }
--- pom.xml/oop/src/main/java/advancedtopics/Zoo.java
+++ pom.xml/oop/src/main/java/advancedtopics/Zoo.java
@@ -1,4 +1,25 @@
 package advancedtopics;
 
+import java.util.Objects;
+
 public class Zoo {
+    String name;
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Zoo zoo = (Zoo) o;
+        return Objects.equals(name, zoo.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    Zoo(String n) {
+        name = n;
+    }
 }
+
+
