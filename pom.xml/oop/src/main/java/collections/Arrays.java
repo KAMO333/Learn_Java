ada493708a12d6a449997c1924641af743eb8a57
Kamogelo <mmopanemoroka@gmail.com>
2025-06-02 14:46:47 +0200

    array list

---
+++ pom.xml/oop/src/main/java/collections/Arrays.java
@@ -0,0 +1,9 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class Arrays {
+    ArrayList color = new ArrayList();
+
+    ArrayList<String> colorsAgain = new ArrayList<String>(10);
+}
