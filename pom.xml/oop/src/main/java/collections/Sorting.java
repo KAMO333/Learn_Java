3f6f7d21653e6d5e096835b4f340f3c15627c9e1
Kamogelo <mmopanemoroka@gmail.com>
2025-06-03 22:28:39 +0200

    sorting integers

--- pom.xml/oop/src/main/java/collections/Sorting.java
+++ pom.xml/oop/src/main/java/collections/Sorting.java
@@ -5,15 +5,25 @@ import java.util.Collections;
 
 public class Sorting {
 
+//    public static void main(String[] args) {
+//        ArrayList<String> animals = new ArrayList<String>();
+//
+//        animals.add("tiger");
+//        animals.add("lion");
+//        animals.add("panda");
+//
+//        Collections.sort(animals);
+//
+//        System.out.println(animals);
+//    }
+
     public static void main(String[] args) {
-        ArrayList<String> animals = new ArrayList<String>();
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
 
-        animals.add("tiger");
-        animals.add("lion");
-        animals.add("panda");
-
-        Collections.sort(animals);
-
-        System.out.println(animals);
+        numbers.add(43);
+        numbers.add(32);
+        numbers.add(23);
+        Collections.sort(numbers);
+        System.out.println(numbers);
     }
 }
