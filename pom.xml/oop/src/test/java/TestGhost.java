8f5aa0a1e1db799eda0b0d309bf3823bf86e52bd
Kamogelo <mmopanemoroka@gmail.com>
2025-06-03 16:46:24 +0200

    tests created

--- pom.xml/oop/src/test/java/TestGhost.java
+++ pom.xml/oop/src/test/java/TestGhost.java
@@ -1,2 +1,13 @@
+import org.junit.Test;
+import practice.Ghost;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestGhost {
+    @Test
+    public void testObjectColor() {
+        Ghost ghost = new Ghost();
+        assertEquals("red", "red");
+        assertEquals("yellow", "yellow");
+    }
 }
