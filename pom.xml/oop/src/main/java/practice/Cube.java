e5923e8e947e6eac4b850a1707ff006fe3c1aef1
Kamogelo <mmopanemoroka@gmail.com>
2025-06-04 18:17:23 +0200

    cubes two

--- pom.xml/oop/src/main/java/practice/Cube.java
+++ pom.xml/oop/src/main/java/practice/Cube.java
@@ -1,13 +1,29 @@
 package practice;
 
 public class Cube{
-    private int sides = 0;
+    private int sides;
+
+    public Cube() {
+        this.sides = 0;
+    }
+
+    public Cube(int number) {
+        convertNegativeToPositive(number);
+    }
 
     public int getSide() {
         return sides;
     }
 
     public void setSide(int number) {
-        this.sides = number;
+        convertNegativeToPositive(number);
+    }
+
+    public void convertNegativeToPositive(int number) {
+        if(number < 0) {
+            this.sides = number * -1;
+        } else {
+            this.sides = number;
+        }
     }
 }
