ffab9b752590844a86a1470688b3aacbd1ba1c08
Kamogelo <mmopanemoroka@gmail.com>
2025-06-05 15:23:22 +0200

    new challenge

---
+++ pom.xml/oop/src/main/java/practice/NameMe.java
@@ -0,0 +1,12 @@
+package practice;
+
+public class NameMe {
+    public String firstName;
+    public String lastName;
+    public String fullName;
+
+    public NameMe(String first, String last) {
+        this.firstName = first;
+        this.lastName = last;
+    }
+}
