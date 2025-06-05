f4d1923c6259363e771a66245cf016da85547b90
Kamogelo <mmopanemoroka@gmail.com>
2025-06-05 23:24:33 +0200

    files reading

--- pom.xml/oop/src/main/java/files/FileClass.java
+++ pom.xml/oop/src/main/java/files/FileClass.java
@@ -1,15 +1,23 @@
 package files;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class FileClass {
     public static void main(String[] args) {
         File file = new File("src/main/resources/File.txt");
 
-        if (file.exists()) {
-            System.out.println(file.getName() + " exists!");
-        } else {
-            System.out.println("The file does not exist");
+//        if (file.exists()) {
+//            System.out.println(file.getName() + " exists!");
+//        } else {
+//            System.out.println("The file does not exist");
+//        }
+
+        try {
+            Scanner read = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
 }
