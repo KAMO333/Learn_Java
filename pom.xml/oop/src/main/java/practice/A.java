4c257ef6e1968606560fc6fb3d9961eef63a1d25
Kamogelo <mmopanemoroka@gmail.com>
2025-06-06 21:17:22 +0200

    A

---
+++ pom.xml/oop/src/main/java/practice/A.java
@@ -0,0 +1,11 @@
+package practice;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class A {
+    @Test
+    void testA() {
+        assertEqual(1, A.getNumber());
+    }
+}
