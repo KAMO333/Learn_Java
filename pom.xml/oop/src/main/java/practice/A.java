352a2d4ba3af7386eb1befa119b7388f6fadd2b7
Kamogelo <mmopanemoroka@gmail.com>
2025-06-06 21:23:07 +0200

    B

--- pom.xml/oop/src/main/java/practice/A.java
+++ pom.xml/oop/src/main/java/practice/A.java
@@ -1,11 +1,7 @@
 package practice;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 public class A {
-    @Test
-    void testA() {
-        assertEqual(1, A.getNumber());
+    public static int getNumber() {
+        return 1;
     }
 }
--- pom.xml/oop/src/test/java/ATest.java
+++ pom.xml/oop/src/test/java/ATest.java
@@ -1,2 +1,16 @@
-public class ATest {
+
+import org.junit.jupiter.api.Test;
+import practice.A;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class ATest {
+    @Test
+    void basicTests() {
+        assertEquals(1, A.getNumber());
+        assertEquals(2, A.getNumber());
+        assertEquals(4, A.getNumber());
+        assertEquals(8, A.getNumber());
+        assertEquals(16, A.getNumber());
+    }
 }
